e035388e814a727c815076b9bd72d2d7587e1cc9
unknown <theusjt49@gmail.com>
2020-02-03 16:22:09 -0600

    I finished the simulation class that can generate a new market firm and created a factory class to allow for the choice between the stack and queue sweep stakes manager.

---
+++ Project SweepStakes/FactoryManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_SweepStakes
+{
+    class FactoryManager
+    {
+       public ISweepStakesManager SetManager(string ManagementType)
+       {
+            Simulation simulation = new Simulation();
+            ManagementType = simulation.ChooseManager();
+            ISweepStakesManager manager;
+            manager = null;
+            switch(ManagementType)
+            {
+                case "Queue":
+                    manager = new SweepStakesQueueManager();
+                    break;
+                case "Stack":
+                    manager = new  SweepSkatesStackManager();
+                    break;
+                default:
+                    Console.WriteLine("please input correct choice");
+                    break;
+
+            }
+            return manager;
+
+            
+       }
+      
+    }
+}
--- Project SweepStakes/Simulation.cs
+++ Project SweepStakes/Simulation.cs
@@ -8,16 +8,39 @@ namespace Project_SweepStakes
 {
     class Simulation
     {
-        ISweepStakesManager manager;
-        public void CreatingMarketingFirmWithManager()
+
+        
+        public void CreatingMarketingFirmWithManager(ISweepStakesManager manager)
         {
             Marketing_Firm marketing_Firm;
             marketing_Firm = new Marketing_Firm(manager);
             
-            // choose btween two managers in simulation?
-
-            
-            
         }
+
+        public string ChooseManager()
+        {
+            Console.WriteLine("What manager do you want Q or S ");
+            string  input = Console.ReadLine();
+            return input;
+        }
+
+        public ISweepStakesManager AddManager(string ManagerType)
+        {
+            FactoryManager factoryManager = new FactoryManager();
+            ISweepStakesManager manager = factoryManager.SetManager(ManagerType);
+            return manager;
+        }
+
+        public void RunSimulation()
+        {
+            string input = ChooseManager();
+            ISweepStakesManager manager = AddManager(input);
+            CreatingMarketingFirmWithManager(manager);
+        }
+        
+
+
+
     }
 }
+ 
